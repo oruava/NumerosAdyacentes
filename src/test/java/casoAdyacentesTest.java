379b36811b61a24ccf9002806058dec87bd4b9bc
Arturo Avalos <137134833+oruava@users.noreply.github.com>
2023-09-22 22:42:20 -0300

    Add files via upload
    4 pruebas relevantes para Actividad 2 Probando el codigo

--- src/test/java/casoAdyacentesTest.java
+++ src/test/java/casoAdyacentesTest.java
@@ -1,23 +1,27 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class casoAdyacentesTest {
 
-    @BeforeEach
+    @org.junit.jupiter.api.BeforeEach
     void setUp() {
     }
-    @AfterEach
+
+    @org.junit.jupiter.api.AfterEach
     void tearDown() {
     }
 
     @Test
     void productoAdyacentes() {
         int[] arreglo = {1, 0, 3, 7, 0, 5};
-        int resultado = casoAdyacentes.productoAdyacentes(arreglo);
-        assertEquals(21, resultado);
-
+        int[] arreglo2 = {-3,2,-5,1,-8,2};
+        int[] arreglo3 = {2,2,2,2,2};
+        int[] arreglo4 = {7,3};
+        assertEquals(21, casoAdyacentes.productoAdyacentes(arreglo));
+        assertEquals(-5, casoAdyacentes.productoAdyacentes(arreglo2));
+        assertEquals(4, casoAdyacentes.productoAdyacentes(arreglo3));
+        assertEquals(21 , casoAdyacentes.productoAdyacentes(arreglo4));
     }
 }
